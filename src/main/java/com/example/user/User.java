3440da654dd819b8834c941b29282a62711eed63
eomjunseong <hi_man159@naver.com>
2022-06-06 15:28:50 +0900

    Filter,Version

--- src/main/java/com/example/user/User.java
+++ src/main/java/com/example/user/User.java
@@ -1,5 +1,8 @@
 package com.example.user;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,6 +14,8 @@ import java.util.Date;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+//@JsonIgnoreProperties(value={"password"})
+@JsonFilter("UserInfo")
 public class User {
 
     private Integer id;
@@ -20,5 +25,7 @@ public class User {
     @Past
     private Date  joinDate;
 
+    private String password;
+    private String ssn;
 
 }
